0acd07836739ab033a0cef194e6e95596753023b
Syfuddin Siam <syfuddinsyam0@gmail.com>
2025-06-18 11:38:54 +0600

    Add Day 1 variables.py

---
+++ day1_variables.py
@@ -0,0 +1,19 @@
+# Day 1: Python Variables & Data Types
+
+name = "Syfuddin"
+age = 22
+height = 5.8
+is_student = True
+
+a = 10
+b = 3
+
+print("Name:",name,"Age:",age,"Height:",height,"Is student:",is_student)
+
+print(a + b, a - b, a * b, a / b, a % b, a ** b)
+
+print(type(name), type(age),type(height),type(is_student))
+
+print("My name is",name,"and I am",age,"years old.") # কমা দিয়ে যুক্ত করা হয়েছে। চাইলে এটা ব্যাবহার না করে নিচের টা ব্যাবহার করতে পারি।
+
+print(f"My name is {name} and I am {age} years old.") # f-string ব্যাবহার করা হয়েছে।
